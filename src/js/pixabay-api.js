f0eaefb54c4719690cec10aed351141aa6d77fa1
Trtl <47746034+Olegka-V@users.noreply.github.com>
2024-04-04 21:00:42 +0300

    finish searcher

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,22 @@
+import { createImg } from "./render-functions";
+
+
 export function getImages(query) {
     const BASE_URL = "https://pixabay.com/";
     const END_POINT = "api/";
     const API_KEY = "43200126-fe47e1a89238cebb9641f8f5f";
     const params = new URLSearchParams({
-        symbol : `${query}`,
+        key: API_KEY,
+        q: query,
+        image_type: "photo",
+        orientation:"horizontal",
+        safesearch: "true",
     });
-    const url = `${BASE_URL}${END_POINT}?key=${API_KEY}&q=${params}&image_type=photo&orientation=horizontal&safesearch=true`;
+    const url = `${BASE_URL}${END_POINT}?${params}`;
 
     return fetch(url).then(res => res.json());
 }
+
+
+
 
